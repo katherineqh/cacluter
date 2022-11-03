899649ce8b5d691bccf3e673dd2b5d06aecdff40
katherine <katherineqh71@gmail.com>
2022-11-03 21:04:03 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// const weight =50
+// const height =1.8
+// const weight =prompt('Please enter your weight')
+// const height =prompt('Please enter your weight')
+// const BMI =weight/(height*weight);
+// console.log(`your BMI is${BMI}`);
+
+let a=[1]
+let b =a
+a=[6,66]
+// a.push(2)
+console.log(b);
